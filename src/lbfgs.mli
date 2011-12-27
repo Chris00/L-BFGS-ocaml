e997c37ee8135a8f5bf1a9b954ac9b4c82176469
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-12-27 15:30:35 +0100

    Rename "usual" F77 library functions so they do not clash with those of other bindings when used simultaneously.

--- src/lbfgs.mli
+++ src/lbfgs.mli
@@ -82,11 +82,13 @@ sig
     ?n:int ->
     ?ofsl:int -> ?l:vec -> ?ofsu:int -> ?u:vec ->
     (vec -> vec -> float) -> ?ofsx:int -> vec -> float
-(** [min f_df x] compute the minimum of the function [f] given by
+  (** [min f_df x] compute the minimum of the function [f] given by
     [f_df].  [x] is an initial estimate of the solution vector.  On
     termination, [x] will contain the best approximation found.  [f_df
     x df] is a function that computes f(x) and its gradiant f'(x),
-    returns f(x) and stores f'(x) in [df].  Can raise {!Abnormal}.
+    returns f(x) and stores f'(x) in [df].  The [x] passed to [f_df x
+    df] is physically equal to the [x] given in [min f_df x].  Can
+    raise {!Abnormal}.
 
     @param l lower bound for each component of the vector [x].  Set
     [l.(i)] to [neg_infinity] to indicate that no lower bound is desired.
